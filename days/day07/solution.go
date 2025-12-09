b628616e0c6f0a7c46ba378cf3573ea7e0e04513
Alex Johnson <alex.johnson@ramseysolutions.com>
2025-12-09 06:45:54 -0600

    [WIP]: Tue Dec  9 06:45:54 CST 2025

--- days/day07/solution.go
+++ days/day07/solution.go
@@ -1,7 +1,15 @@
 package day07
 
+import (
+	"aoc/internal/util"
+	"log"
+)
+
 func Part1(input string) int {
-	// TODO
+	manifold := util.ReadArray(input)
+
+	printManifold(manifold)
+
 	return 0
 }
 
@@ -10,3 +18,15 @@ func Part2(input string) int {
 	return 0
 }
 
+func printManifold(manifold []string) {
+	for _, s := range manifold {
+		log.Println(s)
+	}
+}
+
+func moveBeam(manifold []string) {
+	printManifold(manifold)
+
+	for _, s := range manifold {
+	}
+}
